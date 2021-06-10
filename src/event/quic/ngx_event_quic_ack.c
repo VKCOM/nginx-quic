634c619e7f5a9f6bb7c271a0e6363c1325fbcb30
Andrey Kolyshkin <a.kolyshkin@corp.vk.com>
2021-06-10 12:54:07 +0300

    QUIC: ngx_quic_handle_ack_frame_range optimization.
    Since all sent packets are sorted in a linked list, we can stop after
    the necessary packets are processed in order to avoid processing the
    entire linked list. This is necessary to make it possible to increase
    the congestion window.
    
    before (max in_flight 1mb):
    3.78%  nginx               [.] ngx_quic_handle_ack_frame_range
    
    after (max in_flight 1mb):
    0.27%  nginx               [.] ngx_quic_handle_ack_frame_range

--- src/event/quic/ngx_event_quic_ack.c
+++ src/event/quic/ngx_event_quic_ack.c
@@ -223,44 +223,50 @@ ngx_quic_handle_ack_frame_range(ngx_connection_t *c, ngx_quic_send_ctx_t *ctx,
     st->max_pn = NGX_TIMER_INFINITE;
     found = 0;
 
-    q = ngx_queue_last(&ctx->sent);
+    q = ngx_queue_head(&ctx->sent);
 
     while (q != ngx_queue_sentinel(&ctx->sent)) {
 
         f = ngx_queue_data(q, ngx_quic_frame_t, queue);
-        q = ngx_queue_prev(q);
+        q = ngx_queue_next(q);
 
-        if (f->pnum >= min && f->pnum <= max) {
-            ngx_quic_congestion_ack(c, f);
-
-            switch (f->type) {
-            case NGX_QUIC_FT_ACK:
-            case NGX_QUIC_FT_ACK_ECN:
-                ngx_quic_drop_ack_ranges(c, ctx, f->u.ack.largest);
-                break;
-
-            case NGX_QUIC_FT_STREAM:
-                ngx_quic_handle_stream_ack(c, f);
-                break;
-            }
-
-            if (f->pnum == max) {
-                st->max_pn = f->last;
-            }
-
-            /* save earliest and latest send times of frames ack'ed */
-            if (st->oldest == NGX_TIMER_INFINITE || f->last < st->oldest) {
-                st->oldest = f->last;
-            }
-
-            if (st->newest == NGX_TIMER_INFINITE || f->last > st->newest) {
-                st->newest = f->last;
-            }
-
-            ngx_queue_remove(&f->queue);
-            ngx_quic_free_frame(c, f);
-            found = 1;
+        if (f->pnum < min) {
+            continue;
         }
+
+        if (f->pnum > max) {
+            break;
+        }
+
+        ngx_quic_congestion_ack(c, f);
+
+        switch (f->type) {
+        case NGX_QUIC_FT_ACK:
+        case NGX_QUIC_FT_ACK_ECN:
+            ngx_quic_drop_ack_ranges(c, ctx, f->u.ack.largest);
+            break;
+
+        case NGX_QUIC_FT_STREAM:
+            ngx_quic_handle_stream_ack(c, f);
+            break;
+        }
+
+        if (f->pnum == max) {
+            st->max_pn = f->last;
+        }
+
+        /* save earliest and latest send times of frames ack'ed */
+        if (st->oldest == NGX_TIMER_INFINITE || f->last < st->oldest) {
+            st->oldest = f->last;
+        }
+
+        if (st->newest == NGX_TIMER_INFINITE || f->last > st->newest) {
+            st->newest = f->last;
+        }
+
+        ngx_queue_remove(&f->queue);
+        ngx_quic_free_frame(c, f);
+        found = 1;
     }
 
     if (!found) {
