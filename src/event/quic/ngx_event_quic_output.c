ad33d974152846ea4a9708eb8a9b70fbf82e849f
Andrey Kolyshkin <a.kolyshkin@corp.vk.com>
2021-08-16 12:50:51 +0300

    QUIC: fixed GSO packets count.

--- src/event/quic/ngx_event_quic_output.c
+++ src/event/quic/ngx_event_quic_output.c
@@ -377,8 +377,10 @@ ngx_quic_create_segments(ngx_connection_t *c, ngx_quic_socket_t *qsock)
             }
 
             p += n;
-            nseg++;
 
+            if (n) {
+                nseg++;
+            }
         } else {
             n = 0;
         }
