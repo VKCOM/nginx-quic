52c1cf38dff40080f492be004fb7b195e018c904
Roman Arutyunyan <arut@nginx.com>
2021-09-21 16:24:33 +0300

    QUIC: send RESET_STREAM in response to STOP_SENDING.
    As per RFC 9000:
    
       An endpoint that receives a STOP_SENDING frame MUST send a RESET_STREAM
       frame if the stream is in the "Ready" or "Send" state.
    
       An endpoint SHOULD copy the error code from the STOP_SENDING frame to
       the RESET_STREAM frame it sends, but it can use any application error code.

--- src/event/quic/ngx_event_quic_streams.c
+++ src/event/quic/ngx_event_quic_streams.c
@@ -1266,6 +1266,7 @@ ngx_int_t
 ngx_quic_handle_stop_sending_frame(ngx_connection_t *c,
     ngx_quic_header_t *pkt, ngx_quic_stop_sending_frame_t *f)
 {
+    ngx_pool_t             *pool;
     ngx_event_t            *wev;
     ngx_connection_t       *sc;
     ngx_quic_stream_t      *qs;
@@ -1295,9 +1296,14 @@ ngx_quic_handle_stop_sending_frame(ngx_connection_t *c,
 
         sc = qs->connection;
 
-        wev = sc->write;
-        wev->error = 1;
-        wev->ready = 1;
+        if (ngx_quic_reset_stream(sc, f->error_code) != NGX_OK) {
+            pool = sc->pool;
+
+            ngx_close_connection(sc);
+            ngx_destroy_pool(pool);
+
+            return NGX_ERROR;
+        }
 
         return ngx_quic_init_stream(qs);
     }
@@ -1307,9 +1313,11 @@ ngx_quic_handle_stop_sending_frame(ngx_connection_t *c,
         qs->connection->sent = (off_t) qs->acked;
     }
 
+    if (ngx_quic_reset_stream(qs->connection, f->error_code) != NGX_OK) {
+        return NGX_ERROR;
+    }
+
     wev = qs->connection->write;
-    wev->error = 1;
-    wev->ready = 1;
 
     if (wev->active) {
         ngx_post_event(wev, &ngx_posted_events);
